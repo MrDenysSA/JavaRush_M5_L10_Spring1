5f10390cb87a510b0604058d972968931102f8e3
DenysSyrotiuk <syrotiuk.den@me.com>
2024-03-28 01:23:20 +0200

    Video2 (15:49) work "delete"
    Signed-off-by: DenysSyrotiuk <syrotiuk.den@me.com>

--- src/main/java/syrotiuk/controller/TaskController.java
+++ src/main/java/syrotiuk/controller/TaskController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.util.Objects.isNull;
 
@@ -26,35 +28,41 @@ public class TaskController {
 
         List<Task> tasks = taskService.getAll((page - 1) * limit, limit);
         model.addAttribute("tasks", tasks);
+        model.addAttribute("current_page", page);
+        int totalPages = (int) Math.ceil(1.0 * taskService.getAllCount() / limit);
+        if (totalPages > 1) {
+            List<Integer> pagesNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+            model.addAttribute("page_numbers", pagesNumbers);
+        }
         return "tasks";
     }
 
     @PostMapping("/{id}")
     public String edit(Model model,
-                     @PathVariable Integer id,
-                     @RequestBody TaskInfo info) {
+                       @PathVariable Integer id,
+                       @RequestBody TaskInfo info) {
         if (isNull(id) || id <= 0) {
             throw new RuntimeException("Invalid id");
         }
 
         taskService.edit(id, info.getDescription(), info.getStatus());
-        return tasks(model, 1,10);
+        return tasks(model, 1, 10);
     }
 
     @PostMapping("/")
     public String add(Model model,
-                    @RequestBody TaskInfo info) {
+                      @RequestBody TaskInfo info) {
         taskService.create(info.getDescription(), info.getStatus());
-        return tasks(model, 1,10);
+        return tasks(model, 1, 10);
     }
 
     @DeleteMapping("/{id}")
     public String delete(Model model,
-                         @PathVariable Integer id) {
+                         @PathVariable("id") Integer id) {
         if (isNull(id) || id <= 0) {
             throw new RuntimeException("Invalid id");
         }
         taskService.delete(id);
-        return tasks(model, 1,10);
+        return tasks(model, 1, 10);
     }
 }
